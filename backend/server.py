7833852c7196c8f9c75772ccdf1879db27b3e69e
aernest05 <alexanderernest2010@gmail.com>
2025-10-02 17:49:42 +0700

    trying to fix server .py

--- backend/server.py
+++ backend/server.py
@@ -2,13 +2,13 @@ from flask import Flask, jsonify,request
 import sentiment_analyser
 from datetime import datetime,timedelta
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
 
 cache = {}
 CACHE_DURATION_HOURS = 24  
-debug = True
 
 def get_cached_analysis(company_name):
     """Get from cache if valid"""
@@ -46,6 +46,7 @@ def api_usage():
     api_count = sentiment_analyser.count_api_usage()
     return jsonify(api_count)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)  # NOT 127.0.0.1
 
